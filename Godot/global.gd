106bf87356f4f03d73210d330e19e9514ee8505b
ssrwpr <spinpun@students.mags.school.nz>
2024-09-03 15:09:58 +1200

    3/09

--- Godot/global.gd
+++ Godot/global.gd
@@ -2,7 +2,7 @@ extends Node
 
 var star = 0
 var health = 3
-var levels = ["res://Level.tscn", "res://Level2.tscn", "res://Level3.tscn", "res://End.tscn"]
+var levels = ["res://Level.tscn", "res://Level2.tscn", "res://Level3.tscn"]
 var next_level = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
